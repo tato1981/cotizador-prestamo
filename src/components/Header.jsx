e268c5b16bd48ee4d8d8191216483919e56d6e45
tato1981 <duber22915@gmail.com>
2024-01-29 18:23:34 -0500

    version 01

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+function Header(){
+
+
+    
+    return( 
+        <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+            Cuanto <span className="text-indigo-500">Dinero</span> Necesitas?
+        </h1>
+    )       
+    
+}
+
+export default Header;
